7003091d942e973a955ef991178de0edf6ec1ce5
Elbek Mirzamakhmudov <elbekmirzamakhmudov@gmail.com>
2024-02-14 19:42:06 +0500

    house class created

--- lib/presentation/components/house.dart
+++ lib/presentation/components/house.dart
@@ -1,7 +1,24 @@
+import 'dart:async';
+import 'dart:ui';
+
 import 'package:eco_game/presentation/pages/game/game.dart';
 import 'package:flame/components.dart';
 import 'package:flame/events.dart';
 
 class Building extends SpriteAnimationGroupComponent with HasGameRef<EcoGame>, KeyboardHandler,DragCallbacks {
+  late final SpriteAnimation myAnimation;
 
+
+  @override
+  FutureOr<void> onLoad() {
+
+    _loadAnimations();
+
+
+    return super.onLoad();
+  }
+
+  void _loadAnimations() {
+    // myAnimation = SpriteAnimation.fromFrameData(game.assets., data)
+  }
 }
